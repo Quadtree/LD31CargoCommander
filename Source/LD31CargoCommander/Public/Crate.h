08bd7529554793a864dfede63055e770bdc78cab
Eric Brown <quadtree@gmail.com>
2014-12-06 00:22:03 -0800

    Crate added

---
+++ Source/LD31CargoCommander/Public/Crate.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/StaticMeshActor.h"
+#include "Crate.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class LD31CARGOCOMMANDER_API ACrate : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
