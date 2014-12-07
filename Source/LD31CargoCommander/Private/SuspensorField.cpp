e9cd3d3f8c50ae0a181e16212e052562bf93b539
Eric Brown <quadtree@gmail.com>
2014-12-07 00:15:37 -0800

    Walls!

--- Source/LD31CargoCommander/Private/SuspensorField.cpp
+++ Source/LD31CargoCommander/Private/SuspensorField.cpp
@@ -2,6 +2,10 @@
 
 #include "LD31CargoCommander.h"
 #include "SuspensorField.h"
+#include "EngineUtils.h"
+#include "Crate.h"
+#include "Components/DestructibleComponent.h"
+#include "PhysXIncludes.h"
 
 void ASuspensorField::Tick(float DeltaSeconds)
 {
@@ -9,6 +13,12 @@ void ASuspensorField::Tick(float DeltaSeconds)
 
 	FVector delta = Destination - GetActorLocation();
 	float moveAmt = MoveSpeed * DeltaSeconds;
+
+	FVector movement(0, 0, 0);
+
+	//movement = delta;
+	//movement.Normalize();
+	//movement *= MoveSpeed / 4;
 	
 	if (delta.Size() > moveAmt)
 	{
@@ -20,6 +30,52 @@ void ASuspensorField::Tick(float DeltaSeconds)
 	else
 	{
 		RootComponent->SetWorldLocation(Destination);
+	}
+
+	if (IsOn)
+	{
+		for (auto i = TActorIterator<ACrate>(GetWorld()); i; ++i)
+		{
+			UDestructibleComponent* Destructible = i->FindComponentByClass<UDestructibleComponent>();
+			TArray<FName> bones;
+
+			Destructible->GetBoneNames(bones);
+
+			for (auto i = bones.CreateIterator(); i; ++i)
+			{
+				FVector pos = Destructible->GetBoneLocation(*i);
+
+				FVector delta = pos - GetActorLocation();
+
+				if (delta.Size2D() < 900)
+				{
+					int32 chunkIdx = Destructible->BoneIdxToChunkIdx(Destructible->GetBoneIndex(*i));
+					if (Destructible->ChunkInfos.Num() > chunkIdx && Destructible->ChunkInfos[chunkIdx].Actor)
+					{
+
+						/*if (Destructible->ChunkInfos[chunkIdx].Actor->getLinearVelocity().magnitude() < 0.001f)
+						{
+							UE_LOG(LLog, Display, TEXT("AWAKEN!!"));
+							Destructible->ChunkInfos[chunkIdx].Actor->setLinearVelocity(PxVec3(0, 0, 100.f));
+						}*/
+
+						//float upMove = 0;
+
+						//if (pos.Z < 800) upMove = 500;
+
+						FVector target = GetActorLocation();
+						target.Z = 800;
+
+						movement = target - pos;
+						movement.Normalize();
+						movement *= 500;
+
+						Destructible->ChunkInfos[chunkIdx].Actor->setLinearVelocity(PxVec3(movement.X, movement.Y, movement.Z));
+
+					}
+				}
+			}
+		}
 	}
 }
 
